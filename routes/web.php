9d1996920853bb02272c05b098b599ffb38c4bc3
user_name <email_id>
2019-12-05 10:33:10 +0700

    header

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('index','zingController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
